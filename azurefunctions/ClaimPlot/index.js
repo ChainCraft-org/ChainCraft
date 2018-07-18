6b363c777f174d2b1743390a94b53c83f3f7431b
David Burela <David.Burela@gmail.com>
2018-07-18 16:25:50 +1000

    initial code to get az function working with Ethereum packages

--- azurefunctions/ClaimPlot/index.js
+++ azurefunctions/ClaimPlot/index.js
@@ -1,17 +1,21 @@
+/// Assumes you are running `truffle develop` in another terminal window
+var Web3 = require("web3");
+
+// import the HD wallet for the admin (using the truffle develop default)
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+    var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+    var rpcEndpoint = "http://127.0.0.1:9545";
+    var rwgWallet = new HDWalletProvider(mnemonic, rpcEndpoint, 0); // unlock account 0
+    console.log("Wallet address: " + rwgWallet.addresses[0]);
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: "Wallet address: " + rwgWallet.addresses[0]
+    };
+
     context.done();
 };
